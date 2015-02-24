4c6dc6a67a3a4f0c36b950bf2d909cb8fa165c19
Simon Mergenthal <simon@gmx.de>
2015-02-24 16:41:39 +0100

    Modified fasta.txt. Added values into the hash

--- fastaparser.pl
+++ fastaparser.pl
@@ -5,6 +5,7 @@ use strict;
 
 my $filename = "fasta.txt";
 my %sequence = ();
+my $key = "";
 
 open(FASTA,"<","$filename") or die "Couldn't open $filename: $!" ;
 
@@ -13,12 +14,17 @@ while (<FASTA>)
     if ($_=~/^>(\w+)\s\w+/)
     {
 	$sequence{$1}="";
+	$key = $1;
     }
-};
+    else
+    {
+	$sequence{$key}=$sequence{$key}."$_" ;
+    }
+}
 
 foreach (keys %sequence)
 {
-    print "$_\n";
+    print "$_\t $sequence{$_} \n";
 }
 
 close FASTA or die "Couldn't close $filename: $!" ;
