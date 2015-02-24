8c5003220978176a6ae29e96ed15b461797dca8f
Simon Mergenthal <simon@gmx.de>
2015-02-24 16:03:45 +0100

    Starting a hash and using the ids as keys. Prints out all keys afterwards.

--- fastaparser.pl
+++ fastaparser.pl
@@ -4,15 +4,21 @@ use warnings;
 use strict;
 
 my $filename = "fasta.txt";
+my %sequence = ();
 
 open(FASTA,"<","$filename") or die "Couldn't open $filename: $!" ;
 
 while (<FASTA>)
 { 
-    if ($_=~/^>/)
+    if ($_=~/^>(\w+)\s\w+/)
     {
-         print "$_" ;
+	$sequence{$1}="";
     }
 };
+
+foreach (keys %sequence)
+{
+    print "$_\n";
+}
 
 close FASTA or die "Couldn't close $filename: $!" ;
