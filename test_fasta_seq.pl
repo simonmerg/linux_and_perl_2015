f7abc69e77ccc06132975dc1bbf299af96deade1
Simon Mergenthal <simon@gmx.de>
2015-02-25 17:08:32 +0100

    Added new test

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -6,10 +6,12 @@ use Test::More;
 
 require_ok ('fasta_seq');
 
-my $ralph = new_ok ('fasta_seq');
+my $ralph = new_ok ('fasta_seq',['ralph']);
 
 can_ok('fasta_seq', 'id');
+
 is($ralph->id,'ralph','get_id');
+
 $ralph->id('willi');
 is($ralph->id,'willi','set_id');
 
